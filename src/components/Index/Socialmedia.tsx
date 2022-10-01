b4c58fb69719f749af1f0516d5a109d1954895f3
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-10-01 16:02:54 +0800

    using old commit

--- src/components/Index/Socialmedia.tsx
+++ src/components/Index/Socialmedia.tsx
@@ -2,39 +2,31 @@ import React from 'react'
 import { BsGithub, BsFacebook, BsInstagram, BsTwitter } from 'react-icons/bs/index.js'
 const Socialmedia = () => {
     return (
-        <div className="">
-            <div className=" w-[125px] ">
-                <a target="_blank" className="" href="https://github.com/chenelias/">
-                    <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
-                        <BsGithub className="mr-[5px]" />
-                        @chenelias
-                    </li>
-                </a>
-            </div>
-            <div className=" w-[135px] ">
-                <a target="_blank" className="" href="https://twitter.com/chenelias5/">
-                    <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
-                        <BsTwitter className="mr-[5px]" />
-                        @chenelias5
-                    </li>
-                </a>
-            </div>
-            <div className=" w-[125px] ">
-                <a target="_blank" className="" href="https://www.facebook.com/people/Elias-Chen/100074943037854/">
-                    <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
-                        <BsFacebook className="mr-[5px]" />
-                        @EliasChen
-                    </li>
-                </a>
-            </div>
-            <div className=" w-[170px] ">
-                <a target="_blank" className="" href="https://www.instagram.com/codechen9810/">
-                    <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
-                        <BsInstagram className="mr-[5px]" />
-                        @codechen9810
-                    </li>
-                </a>
-            </div>
+        <div>
+            <a target="_blank" className="flex" href="https://github.com/chenelias/">
+                <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
+                    <BsGithub className="mr-[5px]" />
+                    @chenelias
+                </li>
+            </a>
+            <a target="_blank" className="flex" href="https://twitter.com/chenelias5/">
+                <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
+                    <BsTwitter className="mr-[5px]" />
+                    @chenelias5
+                </li>
+            </a>
+            <a target="_blank" className="flex" href="https://www.facebook.com/people/Elias-Chen/100074943037854/">
+                <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
+                    <BsFacebook className="mr-[5px]" />
+                    @EliasChen
+                </li>
+            </a>
+            <a target="_blank" className="flex" href="https://www.instagram.com/codechen9810/">
+                <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
+                    <BsInstagram className="mr-[5px]" />
+                    @codechen9810
+                </li>
+            </a>
         </div>
     )
 }
--- src/pages/posts/hello.md
+++ src/pages/posts/hello.md
@@ -1,15 +1,10 @@
 ---
 title: Create website with Astro
-url: /posts/hello
 description: Using Astro to develop can make your website more faster and simple
 author: eliaschen
 date: 2022-10-01
 tags:
-    -fff
+    - Web
 ---
 
 # Create website with Astro
-[hello](https://google.com/)
-```js
-print(helloworld)
-```
