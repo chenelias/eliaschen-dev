cc661b1a616d03c9f56f535a0a82c37cf17569b5
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-10-01 18:33:08 +0800

    change using mdx to write posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import react from '@astrojs/react';
 import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 import addClasses from 'rehype-add-classes';
+import mdx from '@astrojs/mdx';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eliaschen.dev/',
-  integrations: [sitemap(), react(), image(), tailwind()],
+  integrations: [sitemap(), react(), image(), tailwind(), mdx()],
   markdown: {
     extendDefaultPlugins: true,
     rehypePlugins: [[addClasses, {
@@ -21,7 +22,8 @@ export default defineConfig({
       h6: 'font-bold font-mplus',
       img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
       p: 'mb-6',
-      a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500'
+      a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500',
+      syntaxHighlight: 'mt-6'
     }]]
   }
 });
