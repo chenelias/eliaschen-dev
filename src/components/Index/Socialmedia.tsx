b47bcaa3d2be4e9be3b5703626022d0c2c8db986
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-10-02 10:58:24 +0800

    fix socialmedialink from index

--- src/components/Index/Socialmedia.tsx
+++ src/components/Index/Socialmedia.tsx
@@ -3,25 +3,29 @@ import { BsGithub, BsFacebook, BsInstagram, BsTwitter } from 'react-icons/bs/ind
 const Socialmedia = () => {
     return (
         <div>
-            <a target="_blank" className="flex" href="https://github.com/chenelias/">
+            <a target="_blank" className="flex w-[120px]" href="https://github.com/chenelias/">
                 <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
                     <BsGithub className="mr-[5px]" />
                     @chenelias
                 </li>
             </a>
-            <a target="_blank" className="flex" href="https://twitter.com/chenelias5/">
+            <a target="_blank" className="w-[130px] flex" href="https://twitter.com/chenelias5/">
                 <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
                     <BsTwitter className="mr-[5px]" />
                     @chenelias5
                 </li>
             </a>
-            <a target="_blank" className="flex" href="https://www.facebook.com/people/Elias-Chen/100074943037854/">
+            <a
+                target="_blank"
+                className="w-[125px] flex"
+                href="https://www.facebook.com/people/Elias-Chen/100074943037854/"
+            >
                 <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
                     <BsFacebook className="mr-[5px]" />
                     @EliasChen
                 </li>
             </a>
-            <a target="_blank" className="flex" href="https://www.instagram.com/codechen9810/">
+            <a target="_blank" className="w-[165px] flex" href="https://www.instagram.com/codechen9810/">
                 <li className="flex hover:underline hover:underline-offset-8 items-center text-purple-500 font-bold">
                     <BsInstagram className="mr-[5px]" />
                     @codechen9810
