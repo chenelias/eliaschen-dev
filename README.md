af6822ee4e5b27d280f0f21a998a8c3b16b7c90d
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-10-06 10:03:27 +0800

    markdown

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 ![image](https://user-images.githubusercontent.com/76611085/193322902-540e9833-502e-480b-b1ec-31dd4839a813.png)
 
 **This is eliaschen.dev , I share some develop and my works in here.**
+
+
+
+
+```
+$ROOT_PROJECT
+├─public    # Static Assets
+│  ├─posts  # PostsMedia
+│  └─works  # WorksMedia
+└─src       
+    ├─components    # Astro & React components
+    ├─layouts   # Posts & Works page layout
+    ├─pages
+    │  ├─posts  # Posts Floder
+    │  └─works  # Works Floder
+    └─styles    # global.css
+```
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from 'react'
+export function getStateSearch() {}
 
-export const Search = () => {
-  return <div>Search</div>;
-};
+function Search() {
+    const [searchtext, setSearchText] = useState('')
+    document.getElementById('output')
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Search Posts"
+                className="input-search inline-flex justify-center rounded-md border-2 border-zinc-400 p-3 text-xl font-medium shadow-sm hover: dark:hover:bg-zinc-700 hover:bg-orange-100 bg-orange-50 dark:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 dark:focus:ring-offset-zinc-900 focus:ring-offset-orange-50 transition-all"
+                onChange={(e) => {
+                    setSearchText(e.target.value)
+                }}
+            />
+            <div id='output'>{searchtext}</div>
+        </div>
+    )
+}
+
+export default Search
