9bc2260b01d3d326c91e330bae6d3915d98f6abd
EliasChen <elias9988110@gmail.com>
2022-10-14 13:41:17 +0000

    fix Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
-    var inputsearch = document.getElementById("inputsearch").value
-  return (
-    <div>
-        <input id='inputsearch' type="search" placeholder="Search Posts" />
-    </div>
-  )
-}
+  return        <div>
+            <input
+                type="text"
+                placeholder="Search Posts"
+                className="input-search inline-flex justify-center rounded-md border-2 border-zinc-400 p-3 text-xl font-medium shadow-sm hover: dark:hover:bg-zinc-700 hover:bg-orange-100 bg-orange-50 dark:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 dark:focus:ring-offset-zinc-900 focus:ring-offset-orange-50 transition-all"
+            />
+        </div>;
+};
 
-export default Search
+export default Search;
