58993fb9e8e3fb95791218a9ccef1b63dd6ed037
EliasChen <elias9988110@gmail.com>
2022-10-16 05:00:20 +0000

    copy right

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react'
 
 const Search = () => {
-  return<div>
+    return (
+        <div>
             <input
                 type="text"
                 placeholder="Search Posts"
                 className="input-search inline-flex justify-center rounded-md border-2 border-zinc-400 p-3 text-xl font-medium shadow-sm hover: dark:hover:bg-zinc-700 hover:bg-orange-100 bg-orange-50 dark:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 dark:focus:ring-offset-zinc-900 focus:ring-offset-orange-50 transition-all"
             />
-        </div>;
-};
+        </div>
+    )
+}
 
-export default Search;
+export default Search
