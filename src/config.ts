39bce2c2682b43ae87e0cc53ff0dc1f096cbb593
EliasChen <elias9988110@gmail.com>
2022-10-16 04:58:37 +0000

    add copyright and about page

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'EliasChen - Home';
-export const SITE_DESCRIPTION = 'This is EliasChen blog , I share some develop skills in here.';
+export const SITE_TITLE = 'EliasChen Home';
+export const SITE_DESCRIPTION = 'This is EliasChen.dev , I share some develop skills in here.';
 export const HOMEPAGE_URL = 'https://eliaschen-dev.vercel.app/'
