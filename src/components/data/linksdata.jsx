900badee78b4d189823c8e33408a5b7cf61f8588
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-10-24 21:48:54 +0800

    posts section style update

---
+++ src/components/data/linksdata.jsx
@@ -0,0 +1,18 @@
+export const linksdata = [
+    {
+        name: 'github',
+        url: 'https://github.com/chenelias',
+    },
+    {
+        name: 'DEV Community',
+        url: '#',
+    },
+    {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/people/Elias-Chen/100074943037854/',
+    },
+    {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/codechen9810/',
+    },
+]
