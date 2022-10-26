42402b3f2bb4dc00014a3d3f3626bd8b4fab543a
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-10-26 21:31:11 +0800

    copyright

--- src/components/data/NavLink.jsx
+++ src/components/data/NavLink.jsx
@@ -2,4 +2,5 @@ export const NavLinks = [
     { title: 'Blog', url: '/blog' },
     { title: 'Works', url: '/works' },
     { title: 'Links', url: '/links' },
+    { title: 'About', url: '/about' },
 ]
