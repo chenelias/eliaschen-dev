fa2a0715b5892e261a4a08c8e3da3ce7ff54c3b5
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-10-28 23:33:56 +0800

    fix style bug

---
+++ src/components/data/photoshots.jsx
@@ -0,0 +1,34 @@
+export const photoshots = [
+    {
+        src: '../media/eliaschen.jpg',
+        alt: 'eliaschen',
+    },
+    {
+        src: '../media/cat3.jpg',
+        alt: 'cat',
+    },
+    {
+        src: '../media/cat1.jpg',
+        alt: 'cat',
+    },
+    {
+        src: '../media/cat2.jpg',
+        alt: 'cat',
+    },
+    {
+        src: '../media/beach.jpg',
+        alt: 'beach',
+    },
+    {
+        src: '../media/dragon.jpg',
+        alt: 'lion',
+    },
+    {
+        src: '../media/mountain.jpg',
+        alt: 'mountain',
+    },
+    {
+        src: '../media/street.jpg',
+        alt: 'street',
+    },
+]
