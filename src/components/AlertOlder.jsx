b6ee94273e986161cc65a0ca71bd9c1f0d3ef536
EliasChen <elias9988110@gmail.com>
2023-01-03 23:10:54 +0800

    Update AlertOlder.jsx

--- src/components/AlertOlder.jsx
+++ src/components/AlertOlder.jsx
@@ -4,7 +4,7 @@ const AlertOlder = () => {
     return (
         <div className="m-2">
             <div className="bg-red-600 w-full text-lg max-w-2xl p-2 mx-auto text-white font-bold rounded-md mb-10 flex">
-                <p> This site is an old website.</p>
+                <p> This site is an older website.</p>
                 <div className="flex-1"></div>
                 <p className="">
                     &thinsp;{' '}
